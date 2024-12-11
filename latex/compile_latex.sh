692ea39fb31b65d9ac20ce0956a42db9f4e4d6bf
arfeta <arber.fetahu@uzh.ch>
2024-12-11 22:21:29 +0100

    Updated compile_latex.sh for the latex presentation

--- latex/compile_latex.sh
+++ latex/compile_latex.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 pdflatex presentation/presentation.tex
 bibtex presentation/presentation
+pdflatex presentation/presentation.tex
+pdflatex presentation/presentation.tex
 pdflatex report.tex
 bibtex report
